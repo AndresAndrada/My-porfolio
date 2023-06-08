5946bff33d3f47112ff82d9042741abee02c32ff
AndresAndrada <andresandrada1994@gmail.com>
2023-06-08 12:00:49 -0300

    Scroll listo, verificar!"

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,69 +1,121 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-// import { Link } from "react-scroll";
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link } from "react-scroll";
+import logo from '../../img/FOTO CV.png';
 import styles from '../NavBar/NavBar.module.css';
-import img from '../../img/FOTO CV.png';
-import docs from '../../doc/CV.pdf';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-const Navbar = (props) => {
-  const [scroll, setScroll] = useState(false);
-  console.log(scroll, 'SCROLL');
+// import './Navbar.css'
 
-  const handleClick = () => setScroll(!scroll);
+const Navbar = () => {
 
-  const closeMenu = () => setScroll(false);
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
 
-  // const handleHome = () => {
-  // }
+    const closeMenu = () => setClick(false)
 
-  return (
-    <div className={styles.conteiner} id="Inicio">
-      <div className={styles.left}>
-        <li>
-          <a href="/home">
-            <img src={img} className={styles.fotohome} alt="home" />
-          </a>
-        </li>
-        {/* <li><FontAwesomeIcon icon={ faBars } /></li> */}
-      </div>
-      <>
-        {props.location.pathname === "/" ? <div className={styles.right}>
-        </div > :
-          <div className={styles.right}>
-              <li className={styles.navItem}>
-                <Link className={styles.link} to="/about" spy={true} smooth={true} offset={100} duration={500} onClick={closeMenu}>
-                  ACERCA DE MI
-                </Link>
-              </li>
-              <li className={styles.navItem}>
-                <Link className={styles.link} to="/project" spy={true} smooth={true} offset={50} duration={500} onClick={closeMenu}>
-                  PROYECTO
-                </Link>
-              </li>
-              {/* <li className={styles.navItem}>
-                <Link className={styles.link} to="/contact" spy={true} smooth={true} offset={150} duration={500} onClick={closeMenu}>
-                  CONTACTO
-                </Link>
-              </li> */}
-              <li className={styles.navItem}>
-                <a className={styles.link} href={docs}>CURRICULUM VITAE</a>
-              </li>
-              <li className={styles.navItem}>
-                <input type="checkbox" id="check" className={styles.check} />
-                <label for="check" className={styles.checkbtn}>
-                  <FontAwesomeIcon icon={faBars} className={styles.icon} />
-                </label>
-              </li>            
-          </div>
-        }
-      </>
-    </div>
-  );
-};
+    return (
+        <div className={styles.conteiner}>
+            {/* <nav> */}
+              <div className={styles.left}>
+                <a href='/home'>
+                    <img src={logo}  className={styles.fotohome} alt='logo' />
+                </a>
+                {/* <div className={ styles.hamburger } onClick={handleClick}>
+                    {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
+                        : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
+                </div> */}
+              </div>
+              <div className={styles.right}>
+                {/* <ul className={click ? styles.navmenu : styles.navmenu}> */}
+                    <li className={styles.navitem}>
+                        <Link className={styles.link} to='Home' spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Home</Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link className={styles.link} to='About' spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>About</Link>
+                    </li>
+                    <li className={styles.navitem}>
+                        <Link className={styles.link} to='Proyecto' spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Proyectos</Link>
+                    </li>
+                    <li className={styles.navitem}>
+                        <Link className={styles.link} to='Footer' spy={true} smooth={true} offset={-150} duration={500} onClick={closeMenu}>Contacto</Link>
+                    </li>
+                {/* </ul> */}
+              </div>
+            {/* </nav> */}
+        </div>
+    )
+}
 
-export default Navbar;
+export default Navbar
+
+
+
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// // import { Link } from "react-scroll";
+// import styles from '../NavBar/NavBar.module.css';
+// import img from '../../img/FOTO CV.png';
+// import docs from '../../doc/CV.pdf';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+// const Navbar = (props) => {
+//   const [scroll, setScroll] = useState(false);
+//   console.log(scroll, 'SCROLL');
+
+//   const handleClick = () => setScroll(!scroll);
+
+//   const closeMenu = () => setScroll(false);
+
+//   // const handleHome = () => {
+//   // }
+
+//   return (
+//     <div className={styles.conteiner} id="Inicio">
+//       <div className={styles.left}>
+//         <li>
+//           <a href="/home">
+//             <img src={img} className={styles.fotohome} alt="home" />
+//           </a>
+//         </li>
+//         {/* <li><FontAwesomeIcon icon={ faBars } /></li> */}
+//       </div>
+//       <>
+//         {props.location.pathname === "/" ? <div className={styles.right}>
+//         </div > :
+//           <div className={styles.right}>
+//               <li className={styles.navItem}>
+//                 <Link className={styles.link} to="/about" spy={true} smooth={true} offset={100} duration={500} onClick={closeMenu}>
+//                   ACERCA DE MI
+//                 </Link>
+//               </li>
+//               <li className={styles.navItem}>
+//                 <Link className={styles.link} to="/project" spy={true} smooth={true} offset={50} duration={500} onClick={closeMenu}>
+//                   PROYECTO
+//                 </Link>
+//               </li>
+//               {/* <li className={styles.navItem}>
+//                 <Link className={styles.link} to="/contact" spy={true} smooth={true} offset={150} duration={500} onClick={closeMenu}>
+//                   CONTACTO
+//                 </Link>
+//               </li> */}
+//               <li className={styles.navItem}>
+//                 <a className={styles.link} href={docs}>CURRICULUM VITAE</a>
+//               </li>
+//               <li className={styles.navItem}>
+//                 <input type="checkbox" id="check" className={styles.check} />
+//                 <label for="check" className={styles.checkbtn}>
+//                   <FontAwesomeIcon icon={faBars} className={styles.icon} />
+//                 </label>
+//               </li>            
+//           </div>
+//         }
+//       </>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
 
 
 // const [current, setCurrent] = useState("Home");
