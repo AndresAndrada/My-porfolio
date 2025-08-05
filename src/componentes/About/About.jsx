6d5e38a6c927a190c1686d329cbfaef560108c0f
unknown <andresandrada1994@gmail.com>
2025-08-05 15:27:59 -0300

    Change de carousel

--- src/componentes/About/About.jsx
+++ src/componentes/About/About.jsx
@@ -12,19 +12,19 @@ const About = () => {
         <h2 className={style.title}>Acerca de Mí</h2>
         {showDescription
           ? <div className={`${style.container} ${style.fadeOut}`}>
-            <div className={style.aboutContent}>
-              <div className={style.profilePicContainer}>
-                <img src={image} alt="Mi foto" className={style.profilePic} />
-              </div>
-              <div className={style.bioContainer}>
-                <h4 className={style.textH4}>Hola, soy Andrada Andres!</h4>
-                <br />
-                <h4 className={style.textH4}>Soy  un apasionado desarrollador web con experiencia destacada en el desarrollo del lado del front-end y back-end. Mi enfoque se centra en la creación de aplicaciones web innovadoras y eficientes. Poseo habilidades sólidas en las tecnologías clave del ecosistema web, con énfasis en JavaScript, React.js y Node.js</h4>
-                <br />
-                <h4 className={style.textH4}>En mi tiempo libre, disfruto aprendiendo nuevas habilidades y creando proyectos interesantes.</h4>
-                <h4 className={style.textH4}>¡No dudes en contactarme si tienes alguna pregunta o proyecto en el que pueda ayudarte!</h4>
-              </div>
+            {/* <div className={style.aboutContent}> */}
+            <div className={style.profilePicContainer}>
+              <img src={image} alt="Mi foto" className={style.profilePic} />
             </div>
+            <div className={style.bioContainer}>
+              <h4 className={style.textH4}>Hola, soy Andrada Andres!</h4>
+              <br />
+              <h4 className={style.textH4}>Soy  un apasionado desarrollador web con experiencia destacada en el desarrollo del lado del front-end y back-end. Mi enfoque se centra en la creación de aplicaciones web innovadoras y eficientes. Poseo habilidades sólidas en las tecnologías clave del ecosistema web, con énfasis en JavaScript, React.js y Node.js</h4>
+              <br />
+              <h4 className={style.textH4}>En mi tiempo libre, disfruto aprendiendo nuevas habilidades y creando proyectos interesantes.</h4>
+              <h4 className={style.textH4}>¡No dudes en contactarme si tienes alguna pregunta o proyecto en el que pueda ayudarte!</h4>
+            </div>
+            {/* </div> */}
           </div>
           : <div className={`${style.container} ${style.fadeIn}`}>
             <div className={style.bioContainer}>
